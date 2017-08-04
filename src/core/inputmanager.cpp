f667af3a793e6e9448ef33f06cca11cadba657fa
Raywell <artimoshkin@gmail.com>
2017-08-04 20:03:18 +0900

    Factorizing inputmanager

--- src/core/inputmanager.cpp
+++ src/core/inputmanager.cpp
@@ -17,20 +17,18 @@ void InputManager::handle() {
                 break;
                 case GLFW_KEY_W:
                     camera->c_pos += camera->speed * camera->front;
-                    camera->t_pos = camera->c_pos + camera->front;
                 break;
                 case GLFW_KEY_S:
                     camera->c_pos -= camera->speed * camera->front;
-                    camera->t_pos = camera->c_pos + camera->front;
                 break;
                 case GLFW_KEY_A:
                     camera->c_pos -= glm::normalize(glm::cross(camera->front, camera->up)) * camera->speed;
-                    camera->t_pos = camera->c_pos + camera->front;
                 break;
                 case GLFW_KEY_D:
                     camera->c_pos += glm::normalize(glm::cross(camera->front, camera->up)) * camera->speed;
-                    camera->t_pos = camera->c_pos + camera->front;
                 break;
+
+                camera->t_pos = camera->front;
             }
         }
     }
